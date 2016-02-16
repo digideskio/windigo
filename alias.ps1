87072e11dc418d0bfce189eb82552ba8ed51d11f
J. Ky Marsh <jkymarsh@gmail.com>
2016-02-16 15:07:08 -0800

    add vagrant aliases

--- alias.ps1
+++ alias.ps1
@@ -70,3 +70,44 @@ Set-Alias grpo git-remote-prune-origin
 
 function git-status { git status }
 Set-Alias gs git-status
+
+# vagrant aliases
+
+function vagrant-box-list { vagrant box list }
+Set-Alias vbl vagrant-box-list
+
+function vagrant-halt { vagrant halt }
+Set-Alias vh vagrant-halt
+
+function vagrant-plugin-install { vagrant plugin install $args }
+Set-Alias vpi vagrant-plugin-install
+
+function vagrant-plugin-list { vagrant plugin list }
+Set-Alias vpl vagrant-plugin-list
+
+function vagrant-plugin-uninstall { vagrant plugin uninstall $args }
+Set-Alias vpui vagrant-plugin-uninstall
+
+function vagrant-plugin-update { vagrant plugin update $args }
+Set-Alias vpu vagrant-plugin-update
+
+function vagrant-provision { vagrant provision }
+Set-Alias vp vagrant-provision
+
+function vagrant-reload { vagrant reload }
+Set-Alias vr vagrant-reload
+
+function vagrant-resume { vagrant resume }
+Set-Alias ve vagrant-resume
+
+function vagrant-ssh { vagrant ssh }
+Set-Alias vs vagrant-ssh
+
+function vagrant-status { vagrant status }
+Set-Alias vt vagrant-status 
+
+function vagrant-suspend { vagrant suspend }
+Set-Alias vd vagrant-suspend
+
+function vagrant-up { vagrant up }
+Set-Alias vu vagrant-up
